8e681d59c67c754279f8db83bad778733479eebd
aleswebgit <aleswebgit@gmail.com>
2021-07-21 18:50:50 +0200

    descuentos: devolver precio con descuento en el HTML

--- descuentos.js
+++ descuentos.js
@@ -3,3 +3,12 @@ function calcularPrecioConDescuento(precio, descuento) {
   const precioConDescuento = (porcentajePrecioConDescuento * precio) / 100;
   return precioConDescuento;
 }
+
+function recibirPrecioConDescuento() {
+  const precio = document.getElementById('precio').value;
+  const descuento = document.getElementById('descuento').value;
+
+  const precioConDescuento = calcularPrecioConDescuento(precio, descuento);
+  const mensaje = `El precio con descuento es ${precioConDescuento}`;
+  document.getElementById('precioConDescuento').innerText = mensaje;
+}
